7ebb45089aa274b9f60f71f3a5ee1dc96071e056
兔兔不给贴贴 <guoy55448@gmail.com>
2026-06-03 15:00:13 +0800

    style: 删除函数内部多余空行

--- kfifo.c
+++ kfifo.c
@@ -12,40 +12,28 @@ void kfifo_init(kfifo_t *fifo, uint8_t *buffer, uint32_t size)
 uint32_t kfifo_put(kfifo_t *fifo, const uint8_t *data, uint32_t len)
 {
 	uint32_t l;
-
 	len = len < (fifo->size - (fifo->in - fifo->out)) ?
 	      len : (fifo->size - (fifo->in - fifo->out));
-
 	smp_mb();
-
 	l = len < (fifo->size - (fifo->in & fifo->mask)) ?
 	    len : (fifo->size - (fifo->in & fifo->mask));
-
 	memcpy(fifo->buffer + (fifo->in & fifo->mask), data, l);
 	memcpy(fifo->buffer, data + l, len - l);
-
 	smp_wmb();
 	fifo->in += len;
-
 	return len;
 }
 
 uint32_t kfifo_get(kfifo_t *fifo, uint8_t *data, uint32_t len)
 {
 	uint32_t l;
-
 	len = len < (fifo->in - fifo->out) ? len : (fifo->in - fifo->out);
-
 	smp_rmb();
-
 	l = len < (fifo->size - (fifo->out & fifo->mask)) ?
 	    len : (fifo->size - (fifo->out & fifo->mask));
-
 	memcpy(data, fifo->buffer + (fifo->out & fifo->mask), l);
 	memcpy(data + l, fifo->buffer, len - l);
-
 	smp_mb();
 	fifo->out += len;
-
 	return len;
 }
